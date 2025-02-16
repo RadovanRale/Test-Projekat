5bd36700282de24c5f643a949d4fe282e7647bed
Radovan Karan <radovanrasa18c@gmail.com>
2025-02-16 20:59:30 +0100

    kod koji za sad radi

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ const App = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      let allPosts = [];
-      for (let i = 1; i <= 100; i++) {
-        const response = await fetch(`/path/to/json/files/post-${i}.json`);
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+        console.log("Ovo je response: ", response)
         const post = await response.json();
-        allPosts.push(post);
-      }
-      setPosts(allPosts);
+        console.log("Ovo je post: ", post)
+      setPosts(post);
     };
 
     fetchPosts();
@@ -21,6 +19,7 @@ const App = () => {
 
   return (
     <div className="p-4 bg-gray-100 min-h-screen">
+      <p>DE SI JBT</p>
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Post = ({ post }) => {
+  console.log("Jedan post: ", post)
   return (
     <div className="max-w-xl mx-auto p-4 bg-white shadow-md rounded-lg mb-4">
       <h2 className="text-xl font-bold">{post.title}</h2>
